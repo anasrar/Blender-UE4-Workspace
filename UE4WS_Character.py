4638c66d42a4e82c82d097ea60cadb1cc2b68a44
Anas RAR <mynameanasrar@gmail.com>
2020-05-03 13:23:41 +0700

    add layout for character panel

--- UE4WS_Character.py
+++ UE4WS_Character.py
@@ -10,5 +10,70 @@ class PANEL(Panel):
 
     def draw(self, context):
         layout = self.layout
+        preferences = context.preferences.addons[__package__].preferences
 
-        layout.label(text="WIP")
+        col = layout.column()
+        row = col.row()
+        split = row.split(factor=0.6)
+        col = split.column()
+        col.alignment = "RIGHT"
+        col.label(text="Subfolder")
+        split = split.split()
+        col = split.column()
+        col.prop(preferences, "CHAR_Subfolder", text="")
+
+        col = layout.column()
+        row = col.row()
+        split = row.split(factor=0.6)
+        col = split.column()
+        col.alignment = "RIGHT"
+        col.label(text="Overwrite File")
+        split = split.split()
+        col = split.column()
+        col.prop(preferences, "CHAR_OverwriteFile", text="")
+
+        col = layout.column()
+        row = col.row()
+        split = row.split(factor=0.6)
+        col = split.column()
+        col.alignment = "RIGHT"
+        col.label(text="Export Character Option")
+        split = split.split()
+        col = split.column()
+        col.prop(preferences, "CHAR_ExportCharacterOption", text="")
+
+        col = layout.column()
+        row = col.row()
+        split = row.split(factor=0.6)
+        col = split.column()
+        col.alignment = "RIGHT"
+        col.label(text="Character Option")
+        split = split.split()
+        col = split.column()
+        col.prop(preferences, "CHAR_CharacterOption", text="")
+
+        if preferences.exportOption in ["BOTH", "UNREAL"]:
+            col = layout.column()
+            row = col.row()
+            split = row.split(factor=0.6)
+            col = split.column()
+            col.alignment = "RIGHT"
+            col.label(text="Character Skeleton")
+            split = split.split()
+            col = split.column()
+            col.prop(preferences, "CHAR_CharacterSkeleton", text="")
+
+            row = layout.row()
+            row.scale_y = 1.5
+            row.operator("ue4workspace.exportstaticmesh",icon="ARMATURE_DATA", text="Update List Skeleton")
+
+        row = layout.row()
+        row.scale_y = 1.5
+        row.operator("ue4workspace.exportstaticmesh",icon="MESH_CUBE", text="Export")
+
+#  OPERATOR
+
+# operator export
+
+Ops = [
+]
