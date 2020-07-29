d36e928fc41b685f6a5cbbca4e3773d93d782ba7
Anas RAR <mynameanasrar@gmail.com>
2020-07-29 21:08:01 +0700

    add snap ik and fk operator

--- UE4WS_ObjectControlRig.py
+++ UE4WS_ObjectControlRig.py
@@ -40,10 +40,10 @@ class PANEL(Panel):
                 split = split.split()
                 col = split.column()
                 col.prop(activeObject, "[\"" + key + "\"]", text="", slider=True)
-                # row = layout.row(align=True)
-                # row.scale_y = 1.2
-                # row.operator("wm.url_open",icon="SNAP_ON", text="Snap FK>IK")
-                # row.operator("wm.url_open",icon="SNAP_ON", text="Snap IK>FK")
+                row = layout.row(align=True)
+                row.scale_y = 1.2
+                row.operator("ue4workspace.snapfktoik",icon="SNAP_ON", text="Snap FK>IK").boneName = activeObject[key.replace("CR_IK_", "CR_SNAPFK_")]
+                row.operator("ue4workspace.snapiktofk",icon="SNAP_ON", text="Snap IK>FK").boneName = activeObject[key.replace("CR_IK_", "CR_SNAPIK_")]
 
         lookControlBones = [key for key in activeObject.keys() if key.startswith("CR_LOOK_")]
 
@@ -62,3 +62,65 @@ class PANEL(Panel):
                 split = split.split()
                 col = split.column()
                 col.prop(activeObject, "[\"" + key + "\"]", text="", slider=True)
+
+# OPERATOR
+
+class OP_SnapFKtoIK(Operator):
+    bl_idname = "ue4workspace.snapfktoik"
+    bl_label = "Snap FK to IK"
+    bl_description = "Snap FK to IK"
+    bl_options = {"UNDO"}
+
+    boneName: StringProperty()
+
+    @classmethod
+    def poll(self, context):
+        return context.active_object is not None and context.mode == "POSE"
+
+    def execute(self, context):
+        activeObject = context.active_object
+        poseBones = activeObject.pose.bones
+        arrBoneName = self.boneName.split("|")
+
+        for boneName in arrBoneName:
+            tweakBone = poseBones.get(boneName)
+            bone = poseBones.get(tweakBone.name.replace("TWEAK_", "FK_"))
+            bpy.ops.pose.visual_transform_apply()
+            bone.matrix = tweakBone.matrix
+        return {"FINISHED"}
+
+class OP_SnapIKtoFK(Operator):
+    bl_idname = "ue4workspace.snapiktofk"
+    bl_label = "Snap IK to FK"
+    bl_description = "Snap IK to FK"
+    bl_options = {"UNDO"}
+
+    boneName: StringProperty()
+
+    @classmethod
+    def poll(self, context):
+        return context.active_object is not None and context.mode == "POSE"
+
+    def execute(self, context):
+        activeObject = context.active_object
+        poseBones = activeObject.pose.bones
+        arrBoneName = [name.replace("TWEAK_", "FK_") for name in self.boneName.split("|")]
+
+        controlBone = poseBones.get(arrBoneName.pop())
+        poleBone = poseBones.get(arrBoneName.pop())
+        lastBone = poseBones.get(arrBoneName.pop())
+
+        for bone, location in [(controlBone, lastBone.tail), (poleBone, lastBone.head)]:
+            trs = bone.matrix.decompose()
+            rot = trs[1].to_matrix().to_4x4()
+            scale = Matrix.Scale(1, 4, trs[2])
+            bpy.ops.pose.visual_transform_apply()
+            bone.matrix = Matrix.Translation(location) @ (rot @ scale)
+        return {"FINISHED"}
+
+# operator export
+
+Ops = [
+    OP_SnapFKtoIK,
+    OP_SnapIKtoFK
+]
