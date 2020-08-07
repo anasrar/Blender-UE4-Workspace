65a7adbc2080945f4c4843074a882df4d7aa9d39
Anas RAR <mynameanasrar@gmail.com>
2020-08-08 00:28:21 +0700

    #15 assets using object_path instead package

--- PyScript/GetAllImportableAssets.py
+++ PyScript/GetAllImportableAssets.py
@@ -7,4 +7,4 @@ asset_registry = AssetRegistryHelpers.get_asset_registry()
 # get all assets
 all_assets = asset_registry.get_assets_by_path('/Game', recursive=True)
 # filter for skeleton assets
-print(json.dumps([(str(asset.package_name ), str(asset.asset_name), {"StaticMesh": "MESH", "SkeletalMesh": "ARMATURE", "AnimSequence": "ACTION"}[str(asset.asset_class)]) for asset in all_assets if str(asset.asset_class) in ["StaticMesh", "SkeletalMesh", "AnimSequence"]]))
+print(json.dumps([(str(asset.object_path), str(asset.asset_name), {"StaticMesh": "MESH", "SkeletalMesh": "ARMATURE", "AnimSequence": "ACTION"}[str(asset.asset_class)]) for asset in all_assets if str(asset.asset_class) in ["StaticMesh", "SkeletalMesh", "AnimSequence"]]))
