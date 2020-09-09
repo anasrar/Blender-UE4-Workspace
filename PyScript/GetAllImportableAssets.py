bc55aa699d5bca4ed895f09da994469142ed8aeb
Anas RAR <mynameanasrar@gmail.com>
2020-09-09 08:19:34 +0700

    change output list into a file (#18)

--- PyScript/GetAllImportableAssets.py
+++ PyScript/GetAllImportableAssets.py
@@ -1,3 +1,5 @@
+import os
+import inspect
 import json
 from unreal import (
     AssetRegistryHelpers
@@ -6,5 +8,12 @@ from unreal import (
 asset_registry = AssetRegistryHelpers.get_asset_registry()
 # get all assets
 all_assets = asset_registry.get_assets_by_path('/Game', recursive=True)
+
+# Fix Python PATH Script Issue #9
+filename = inspect.getframeinfo(inspect.currentframe()).filename
+currentPath = os.path.dirname(os.path.abspath(filename))
+
+file = open(os.path.normpath(os.path.join(currentPath, "..", "Data", "assetsList.json")), "w+")
 # filter for skeleton assets
-print(json.dumps([(str(asset.object_path), str(asset.asset_name), {"StaticMesh": "MESH", "SkeletalMesh": "ARMATURE", "AnimSequence": "ACTION"}[str(asset.asset_class)]) for asset in all_assets if str(asset.asset_class) in ["StaticMesh", "SkeletalMesh", "AnimSequence"]]))
+file.write(json.dumps([(str(asset.object_path), str(asset.asset_name), {"StaticMesh": "MESH", "SkeletalMesh": "ARMATURE", "AnimSequence": "ACTION"}[str(asset.asset_class)]) for asset in all_assets if str(asset.asset_class) in ["StaticMesh", "SkeletalMesh", "AnimSequence"]]))
+file.close()
